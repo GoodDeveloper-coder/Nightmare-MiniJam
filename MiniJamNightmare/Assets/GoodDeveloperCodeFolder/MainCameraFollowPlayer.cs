b4e48b34ef43d9e8867e535738a03e84be095d82
GoodDeveloper-coder <itmancik@gmail.com>
2023-10-28 12:17:52 +0300

    Update shoot system and more

---
+++ MiniJamNightmare/Assets/GoodDeveloperCodeFolder/MainCameraFollowPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = player.position.x;
+        temp.y = player.position.y;
+
+        transform.position = temp;
+    }
+}
